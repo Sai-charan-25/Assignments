604bf1de11f5320d527211088ce9dcba2de45d25
Naga Sai Charan Bale <bnsc.5594@gmail.com>
2026-04-14 18:33:09 +0530

    hell pgm

---
+++ javascript-training/language-basics/hello-world.js
@@ -0,0 +1,8 @@
+
+console.log("Hello-World");
+let Name = "Sai";
+console.log(`Hello $(Name)`);
+
+console.warn("this is warning message");
+console.error("this is error message");
+console.info("this is information");
