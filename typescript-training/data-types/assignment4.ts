3b7e1a46d0fea43818b54d095fbae686597326a4
Naga Sai Charan Bale <bnsc.5594@gmail.com>
2026-04-29 17:00:54 +0530

    completed all assignments

--- typescript-training/data-types/assignment4.ts
+++ typescript-training/data-types/assignment4.ts
@@ -7,19 +7,44 @@ transactions.set(5, -200);
 transactions.set(6, -300);
 transactions.set(7, 4000);
 transactions.set(8, -3000);
+let creditCount:number=0;
+let debitCount:number=0;
+let totalAmountCredited:number=0;
+let totalAmountDebited:number=0;
+let balanceAmountAvailable:number=0;
 
 console.log("Transcation", transactions);
-
 console.log("total number of credit and debit transcations Completed:", transactions.size);
-
 for(let i:number=1; i<=transactions.size; i++)
 {
     console.log(transactions.get(i));
+    if(transactions.get(i)!>0)
+    {
+        creditCount++;
+        totalAmountCredited += transactions.get(i)!;
+        if(transactions.get(i)!>10000)
+        {
+            console.log(`Suspicious transaction of amount ${transactions.get(i)}`)
+        }
+    }
+    else
+    {
+        debitCount++
+        totalAmountDebited +=transactions.get(i)!;
+        if(transactions.get(i)!<-10000)
+        {
+            console.log(`Suspicious transaction of amount ${transactions.get(i)}`)
+        }
+    }
 
-    
 }
 
-console.log(transactions.entries);
+console.log(`total credited: ${creditCount}`);
+console.log(`total debited: ${debitCount}`);
+console.log(`total credited amount ${totalAmountCredited}`);
+console.log(`total credited amount ${totalAmountDebited}`);
+console.log(`Balance amount in the account ${totalAmountCredited-totalAmountDebited}`);
 
-console.log("total amount credited:", );
-console.log("total amount debited:",)
+
+
+
